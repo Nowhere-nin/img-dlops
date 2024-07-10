874a287cec290797afbfc5505f329d5f866fab76
Michael Sandoval <michael.sandovalm20@gmail.com>
2024-07-10 12:16:11 -0400

    Se está utilizando dvc para trackear los modelos

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import onnxruntime as rt
+import numpy as np
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+# Cargar el modelo ONNX
+sess = rt.InferenceSession("models/mi_modelo1.onnx")
+
+# Define el tamaño de la imagen de entrada
+img_height = 224
+img_width = 224
+
+# Define las categorías
+categories = ['auto', 'casa', 'edificio', 'gato', 'moto']
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    if not file:
+        return jsonify({'error': 'No file provided'}), 400
+
+    # Convertir la imagen a un array numpy
+    img = Image.open(io.BytesIO(file.read())).resize((img_width, img_height))
+    img_array = np.array(img).astype('float32')
+    if len(img_array.shape) == 2:  # Si la imagen es en escala de grises, conviértela a RGB
+        img_array = np.stack([img_array]*3, axis=-1)
+    if img_array.shape[-1] == 4:  # Si la imagen tiene un canal alfa, elimínalo
+        img_array = img_array[:, :, :3]
+    img_array = np.expand_dims(img_array, axis=0)  # Agregar una dimensión para el batch
+
+    # Realizar la predicción
+    input_name = sess.get_inputs()[0].name
+    result = sess.run(None, {input_name: img_array})
+
+    # Obtener la categoría con la probabilidad más alta
+    prediction = np.argmax(result[0], axis=1)[0]
+    print(prediction)
+    predicted_category = categories[prediction]
+
+    return jsonify({'prediction': predicted_category})
+
+if __name__ == '__main__':
+    app.run(debug=True)
